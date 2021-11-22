d53491bc12ffcc149a346f324cbd9d6f2015241c
pavloteyfel <pavloteyfel@gmail.com>
2021-11-22 22:24:00 +0100

    style

--- backend/src/api.py
+++ backend/src/api.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
+# db_drop_and_create_all()
 
 # --------------------------------------------------------------------------- #
 # Validation schemas for payloads based on https://json-schema.org
@@ -25,7 +26,7 @@ create_drinks_schema = {
         'recipe': {
             'type': 'array',
             'items': {
-                'type': 'object', 
+                'type': 'object',
                 'properties': {
                     'name': {'type': 'string'},
                     'color': {'type': 'string'},
@@ -52,7 +53,7 @@ update_drinks_schema = {
                 'recipe': {
                     'type': 'array',
                     'items': {
-                        'type': 'object', 
+                        'type': 'object',
                         'properties': {
                             'name': {'type': 'string'},
                             'color': {'type': 'string'},
@@ -70,13 +71,15 @@ update_drinks_schema = {
 # --------------------------------------------------------------------------- #
 # Routes
 # --------------------------------------------------------------------------- #
+
+
 @app.route('/drinks')
 def get_drinks():
     '''
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where 
+    returns status code 200 and json {"success": True, "drinks": drinks} where
     drinks is the list of drinks
         or appropriate status code indicating reason for failure
     '''
@@ -90,13 +93,14 @@ def get_drinks():
         'drinks': [drink.short() for drink in drinks],
     }), 200
 
+
 @app.route('/drinks-detail')
 def get_drinks_detail():
     '''
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where 
+    returns status code 200 and json {"success": True, "drinks": drinks} where
     drinks is the list of drinks
         or appropriate status code indicating reason for failure
     '''
@@ -110,6 +114,7 @@ def get_drinks_detail():
         'drinks': [drink.long() for drink in drinks],
     }), 200
 
+
 @app.route('/drinks', methods=['POST'])
 @expects_json(create_drinks_schema)
 def create_drinks():
@@ -118,21 +123,22 @@ def create_drinks():
             it should create a new row in the drinks table
             it should require the 'post:drinks' permission
             it should contain the drink.long() data representation
-        returns status code 200 and json {"success": True, "drinks": drink} 
+        returns status code 200 and json {"success": True, "drinks": drink}
         where drink an array containing only the newly created drink
             or appropriate status code indicating reason for failure
     '''
 
     drink = Drink.query.filter(
         Drink.title == payload.data.get('title')).first()
-    
-    # 409 https://stackoverflow.com/questions/12658574/rest-api-design-post-to-create-with-duplicate-data-would-be-integrityerror-500
+
+    # 409
+    # https://stackoverflow.com/questions/12658574/rest-api-design-post-to-create-with-duplicate-data-would-be-integrityerror-500
     if drink:
         abort(409)
 
     drink = Drink(
-        title = payload.data.get('title'),
-        recipe = json.dumps(payload.data.get('recipe'))
+        title=payload.data.get('title'),
+        recipe=json.dumps(payload.data.get('recipe'))
     )
 
     # Detach drink data from the session
@@ -145,6 +151,7 @@ def create_drinks():
         'drinks': [drink_data]
     }), 200
 
+
 @app.route('/drinks/<int:id>', methods=['PATCH'])
 @expects_json(update_drinks_schema)
 def update_drinks(id):
@@ -155,7 +162,7 @@ def update_drinks(id):
             it should update the corresponding row for <id>
             it should require the 'patch:drinks' permission
             it should contain the drink.long() data representation
-        returns status code 200 and json {"success": True, "drinks": drink} 
+        returns status code 200 and json {"success": True, "drinks": drink}
         where drink an array containing only the updated drink
             or appropriate status code indicating reason for failure
     '''
@@ -163,13 +170,13 @@ def update_drinks(id):
     drink = Drink.query.get_or_404(id)
 
     # Updates title property if message contains "title"
-    if 'title' in  payload.data:
+    if 'title' in payload.data:
         drink.title = payload.data.get('title')
 
     # Updates recipe property if message contains "recipe"
-    if 'recipe' in  payload.data:
+    if 'recipe' in payload.data:
         drink.recipe = json.dumps(payload.data.get('recipe'))
-    
+
     # Detach drink data from the session
     drink_data = drink.long()
 
@@ -180,6 +187,7 @@ def update_drinks(id):
         'drinks': [drink_data]
     }), 200
 
+
 @app.route('/drinks/<int:id>', methods=['DELETE'])
 def delete_drinks(id):
     '''
@@ -188,7 +196,7 @@ def delete_drinks(id):
             it should respond with a 404 error if <id> is not found
             it should delete the corresponding row for <id>
             it should require the 'delete:drinks' permission
-        returns status code 200 and json {"success": True, "delete": id} where 
+        returns status code 200 and json {"success": True, "delete": id} where
         id is the id of the deleted record
             or appropriate status code indicating reason for failure
     '''
@@ -206,6 +214,7 @@ def delete_drinks(id):
 # Error handling
 # --------------------------------------------------------------------------- #
 
+
 @app.errorhandler(400)
 def bad_request(error):
     """
@@ -219,21 +228,26 @@ def bad_request(error):
                 {'error': 400, 'message': original_error.message}), 400)
     return jsonify({'error': 400, 'message': 'bad request'}), 400
 
+
 @app.errorhandler(404)
 def resource_not_found(error):
     return jsonify({'error': 404, 'message': 'resource not found'}), 404
+
 
 @app.errorhandler(405)
 def method_not_allowed(error):
     return jsonify({'error': 405, 'message': 'method not allowed'}), 405
 
+
 @app.errorhandler(409)
 def conflict(error):
     return jsonify({'error': 409, 'message': 'resource already exists'}), 409
 
+
 @app.errorhandler(422)
 def unprocessable_entity(error):
     return jsonify({'error': 422, 'message': 'unprocessable entity'}), 422
+
 
 '''
 @TODO implement error handler for AuthError
--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -6,7 +6,8 @@ import json
 
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_filename))
 
 db = SQLAlchemy()
 
@@ -34,15 +35,12 @@ db_drop_and_create_all()
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
-    # add one demo row which is helping in POSTMAN test
-    drink = Drink(
-        title='water',
-        recipe='[{"name": "water", "color": "blue", "parts": 1}]'
-    )
+    drinks = [
+        {'title': 'Water',
+         'recipe': '[{"name": "water", "color": "blue", "parts": 1}]'}]
+    for drink in drinks:
+        Drink(**drink).insert()
 
-
-    drink.insert()
-# ROUTES
 
 '''
 Drink
@@ -56,7 +54,8 @@ class Drink(db.Model):
     # String Title
     title = Column(String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
+    # the required datatype is [{'color': string, 'name':string,
+    # 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
     '''
@@ -65,7 +64,8 @@ class Drink(db.Model):
     '''
 
     def short(self):
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        short_recipe = [{'color': r['color'], 'parts': r['parts']}
+                        for r in json.loads(self.recipe)]
         return {
             'id': self.id,
             'title': self.title,
